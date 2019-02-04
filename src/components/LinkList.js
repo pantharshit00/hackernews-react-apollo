11e09b3c30ac9a6d7c7106794f3f2bec87619550
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-02-04 16:33:51 +0300

    remove the inmemory list

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -17,20 +17,7 @@ const FEED_QUERY = gql`
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: "1",
-        description:
-          "Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎",
-        url: "https://www.prisma.io"
-      },
-      {
-        id: "2",
-        description: "The best GraphQL client",
-        url: "https://www.apollographql.com/docs/react/"
-      }
-    ];
-
+    
     return (
       <Query query={FEED_QUERY}>
         {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
