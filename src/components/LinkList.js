04b9c00e8eb73b97383213c6243d6abb91e5e136
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-02-04 16:49:48 +0300

    use the render prop function to display the list of links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -17,10 +17,21 @@ const FEED_QUERY = gql`
 
 class LinkList extends Component {
   render() {
-    
+
     return (
       <Query query={FEED_QUERY}>
-        {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+        {({loading, error, data}) =>{
+          if (loading) return <div>Loading</div>
+          if (error) return <div>Error</div>
+
+          const linksToRender = data.feed.links
+
+          return(
+            <div>
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )
+        }}
       </Query>
     );
   }
