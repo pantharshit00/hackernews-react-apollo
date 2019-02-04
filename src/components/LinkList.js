55475f73b26d15b296c9948727be3d72a898fd4e
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-02-04 16:21:46 +0300

    import the necessary dependencies

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
-import Link from './Link'
+import React, { Component } from "react";
+import Link from "./Link";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 const FEED_QUERY = gql`
   {
-    feed{
-      links{
+    feed {
+      links {
         id
         description
         url
@@ -17,23 +19,24 @@ class LinkList extends Component {
   render() {
     const linksToRender = [
       {
-        id: '1',
-        description: 'Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎',
-        url: 'https://www.prisma.io',
+        id: "1",
+        description:
+          "Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎",
+        url: "https://www.prisma.io"
       },
       {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
+        id: "2",
+        description: "The best GraphQL client",
+        url: "https://www.apollographql.com/docs/react/"
+      }
+    ];
 
     return (
-      <Query query = {FEED_QUERY}>
-      {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+      <Query query={FEED_QUERY}>
+        {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
       </Query>
-    )
+    );
   }
 }
 
-export default LinkList
+export default LinkList;
