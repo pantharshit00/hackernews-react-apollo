6417985982a1570a9bdddfb2931bb06e720a4ef3
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-02-05 14:15:10 +0300

    Add the necessary dependencies

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
